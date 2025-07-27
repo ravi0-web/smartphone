7c4faf2bce565408b955d5656bf834d93aa4c7c8
ravi0-web <ravi.01022004g@gmail.com>
2025-07-27 16:49:56 +0530

    fix allowed hosts

--- stats/settings.py
+++ stats/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-_50fo)(%8y39rgwl$p-#_671jhb&n4rg-#xxukf)xvko=bup%+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['smartphone-production.up.railway.app']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
@@ -83,7 +83,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stats.wsgi.application'
+WSGI_APPLICATION = 'stats.wsgi.application','web: gunicorn stats.wsgi'
 
 
 # Database
